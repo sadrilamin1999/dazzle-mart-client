df0dd434f93ba9811b98f6f8ada2864f19250868
sadrilamin1999 <sadrilshuvo420@gmail.com>
2023-07-19 17:36:25 +0600

    animation added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,71 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { getTransition } from "@/utils/getTransition";
+
 const Hero = () => {
   return (
-    <section className="wraper section-padding h-[calc(100vh-5rem)]">
-      Hero
+    <section className="wrapper section-padding h-[calc(100vh-5rem)] grid grid-cols-5">
+      <div className="w-full h-full">
+        <div className="w-full h-4/6 overflow-hidden">
+          <motion.div
+            initial={{ y: "-100%" }}
+            whileInView={{ y: "0" }}
+            transition={getTransition()}
+            className="w-full h-full"
+          >
+            <Image
+              src="https://images.pexels.com/photos/4982737/pexels-photo-4982737.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={300}
+              height={700}
+              priority
+              className=" w-full h-full object-cover"
+            />
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-full h-full"></div>
+      <div className="w-full h-full flex items-center">
+        <div className="w-full h-4/6 overflow-hidden">
+          <motion.div
+            initial={{ y: "-100%" }}
+            whileInView={{ y: "0" }}
+            transition={getTransition(0.2)}
+            className="w-full h-full"
+          >
+            <Image
+              src="https://images.pexels.com/photos/3925875/pexels-photo-3925875.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={300}
+              height={700}
+              priority
+              className=" w-full h-full object-cover"
+            />
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-full h-full"></div>
+      <div className="w-full h-full flex items-end">
+        <div className="w-full h-4/6 overflow-hidden">
+          <motion.div
+            initial={{ y: "-100%" }}
+            whileInView={{ y: "0" }}
+            transition={getTransition(0.4)}
+            className="w-full h-full"
+          >
+            <Image
+              src="https://images.pexels.com/photos/8185815/pexels-photo-8185815.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              alt=""
+              width={300}
+              height={700}
+              priority
+              className=" w-full h-full object-cover"
+            />
+          </motion.div>
+        </div>
+      </div>
     </section>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,19 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { BsBag } from "react-icons/bs";
 import Link from "next/link";
 import Button from "./Button";
+import { getTransition } from "@/utils/getTransition";
 
 const Navbar = () => {
   return (
-    <header className="h-20 fixed top-0 left-0 right-0 bg-light/90 backdrop-blur-lg border-b border-dark-10">
+    <motion.header
+      initial={{ y: "-100%" }}
+      animate={{ y: 0 }}
+      transition={getTransition(0.6)}
+      className="h-20 fixed top-0 left-0 right-0 bg-light/90 backdrop-blur-lg border-b border-dark-10"
+    >
       <div className="wrapper w-full h-full flex justify-between items-center">
         {/* Nav left */}
         <nav>
@@ -37,9 +47,17 @@ const Navbar = () => {
           </ul>
         </nav>
         {/* Nav right */}
-        <Button href="/login" placeholder="Login" color="green" />
+        <nav className="flex items-center gap-5">
+          <Link href="/cart" className="relative">
+            <BsBag />
+            <span className=" absolute text-xs w-4 h-4 rounded-full bg-rose-500 text-light flex justify-center items-center -right-2 -bottom-2">
+              10
+            </span>
+          </Link>
+          <Button href="/login" placeholder="Login" color="green" />
+        </nav>
       </div>
-    </header>
+    </motion.header>
   );
 };
 
